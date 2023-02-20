79e4f75519883413f3ac8ce727241316079b52b0
UP2013395 <91505266+UP2013395@users.noreply.github.com>
2023-02-20 18:53:40 +0000

    Start

---
+++ Code/template_loading.js
@@ -0,0 +1,13 @@
+function clearBody(area) {
+    const pageArea = document.querySelector(area);
+    while (pageArea.hasChildNodes()) {
+        pageArea.removeChild(pageArea.firstChild);
+    }
+}
+
+function loadPage(pageID) {
+    const newPage = document.querySelector(`#${pageID}`);
+    clearBody("#pageArea");
+    const newContent = newPage.content.cloneNode(true);
+    document.querySelector("#pageArea").appendChild(newContent);
+}
