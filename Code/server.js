e9431ea72cbf51dacb23149809907c40bee7ac15
UP2013395 <91505266+UP2013395@users.noreply.github.com>
2023-03-14 20:27:35 +0000

    Changing CSS, also starting on the Auth0 code

--- Code/server.js
+++ Code/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-
+import { join } from "path";
 const app = express();
 
-app.use(express.static("Code", { extensions: ['html'] }));
 
-app.listen(8081);
-console.log("Server listening on port 8081");
+app.use(express.static("Code", { extensions: ['html'] }));
+app.get("/auth_config.json", (req, res) => { res.sendFile(join(__dirname, "auth_config.json")) });
+app.listen(8081, () => console.log("Server listening on port 8081"));
