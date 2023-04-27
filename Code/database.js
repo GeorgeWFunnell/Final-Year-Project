4f1225379e230ceaae2dee29a700e93598728802
UP2013395 <91505266+UP2013395@users.noreply.github.com>
2023-04-27 13:18:06 +0100

    eror fixings

--- Code/database.js
+++ Code/database.js
@@ -3,18 +3,10 @@ import sqlite3 from 'sqlite3';
 
 
 
-// A function that opens the database and makes sure its up to date
 async function init() {
+
     const db = await open({ filename: "./app/Diary_Database.sqlite", driver: sqlite3.Database });
     await db.migrate({ migrationsPath: "./app/Migrations" });
-    await db.exec(`
-    CREATE TABLE IF NOT EXISTS DIARY (
-      ID INTEGER PRIMARY KEY AUTOINCREMENT,
-      Title TEXT,
-      Date DATE,
-      Diary Text
-    )
-  `);
     return db;
 }
 
